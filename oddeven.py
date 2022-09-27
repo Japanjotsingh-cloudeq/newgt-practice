04a601303e4ad175a0b1cb72d1aa3969b57fc334
japanjot15 <79826990+japanjot15@users.noreply.github.com>
2022-09-27 12:31:37 +0530

    t6

--- oddeven.py
+++ oddeven.py
@@ -3,9 +3,10 @@ import sys
 a = int(sys.argv[1])
 b = int(sys.argv[2])
 
-for x in range(a,b):
-    if(x%2!=0):
-        print("even",x)
-    else:
-        print("odd")
-           
+# for x in range(a,b):
+#     if(x%2!=0):
+#         print("even",x)
+#     else:
+#         print("odd")
+sum = a + b
+print(sum)
