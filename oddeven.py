71a091172a21f560d7e3af4c811234d4c56941ff
japanjot15 <79826990+japanjot15@users.noreply.github.com>
2022-09-27 12:22:18 +0530

    t4

--- oddeven.py
+++ oddeven.py
@@ -2,12 +2,10 @@ import sys
 
 a = int(sys.argv[1])
 b= int(sys.argv[2])
-oddlist=[]
-evenlist=[]
+
 for x in range(a,b):
     if(x%2!=0):
-        oddlist.append(x)
+        print("even")
     else:
-        evenlist.append(x)
-print("odd list", oddlist)            
-print("even list",evenlist) 
+        print("odd")
+           
